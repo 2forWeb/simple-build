17d4e6e2a2ceed75556f8ecbe4d64c5d05b89d39
shadowc <info@matiasjose.com>
2025-03-06 01:04:24 +0100

    fix eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,11 @@
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import globals from 'globals';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {
-    platforms: ["node"],
-  },
+  {files: ['**/*.{js,mjs,cjs,ts}']},
+  {languageOptions: { globals: globals.node }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
 ];
