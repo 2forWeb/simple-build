621c163ff228ca141f03e3734e84c78fb53ec8f4
shadowc <info@matiasjose.com>
2026-01-22 23:53:07 +0100

    fix linters

--- src/client/components/ux/icons/FormIcon.vue
+++ src/client/components/ux/icons/FormIcon.vue
@@ -18,12 +18,12 @@
             stroke-linecap="round"
             stroke-linejoin="round"
         />
-        <path d="M6 14H40" stroke="#000000" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
-        <path d="M30 22H42" stroke="#000000" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
-        <path d="M30 30H42" stroke="#000000" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
-        <path d="M23 22H24" stroke="#000000" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
-        <path d="M23 30H24" stroke="#000000" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
-        <path d="M14 6L14 42" stroke="#000000" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M6 14H40" stroke="#000000" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" />
+        <path d="M30 22H42" stroke="#000000" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" />
+        <path d="M30 30H42" stroke="#000000" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" />
+        <path d="M23 22H24" stroke="#000000" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" />
+        <path d="M23 30H24" stroke="#000000" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" />
+        <path d="M14 6L14 42" stroke="#000000" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" />
     </svg>
 </template>
 
